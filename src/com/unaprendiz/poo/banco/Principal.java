d857ec12c08badc221dfbfc9ce0c8f39376280ad
MayccSuarez <mrsmaycol@gmail.com>
2016-02-09 10:24:16 -0500

    Es la encargada de ejecutar la aplicación.

---
+++ src/com/unaprendiz/poo/banco/Principal.java
@@ -0,0 +1,12 @@
+package com.unaprendiz.poo.banco;
+
+public class Principal
+{
+
+	public static void main(String[] args)
+	{
+		Cuenta c = new Cuenta();
+		
+		c.menu(); //método que contiene el menú de la aplicación
+	}
+}
