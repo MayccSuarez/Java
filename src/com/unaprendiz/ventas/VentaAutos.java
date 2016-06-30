20fe94568fdf5624b8cb0bca2b2f2cf6572b4c23
MayccSuarez <mrsmaycol@gmail.com>
2016-06-30 15:47:25 -0500

    Descripción del problema de venta de autos usados

---
+++ src/com/unaprendiz/ventas/VentaAutos.java
@@ -0,0 +1,22 @@
+
+package com.unaprendiz.ventas;
+
+/**
+ *
+ * @author raptor
+ */
+public class VentaAutos {
+    
+    /* Una compañia se dedica a la de venta de carros usados, paga a su personal de ventas un salario 
+    de 500 dolares mensuales, más una comisión de 20 dolares por cada carro vendido, 
+    más el 5% del valor de la venta por carro.
+    Cada mes la secretaria de la empresa ingresa en la computadora los datos pertinentes.
+    Diseñar y codificar un algoritmo que calcule e imprima el salario mensual de un vendedor dado. 
+    */
+    
+    public static void main(String[] args) {
+        
+   
+        
+    }      
+}
